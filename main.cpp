2819ffcf047aec50630ce297d9e33da66575fdaa
caraxin <xinxu129@gmail.com>
2017-01-22 13:24:09 -0800

    delete configparser submodule and add the config_parser.cc/.h directly in the repo

--- main.cpp
+++ main.cpp
@@ -2,23 +2,37 @@
 #include <string>
 #include <boost/asio.hpp>
 #include "server.hpp"
+#include "config_parser.h"
+
+void config_handler(NginxConfig* config, std::string& port) {
+  for(auto statement: config->statements_) {
+    if(statement->child_block_ != NULL) config_handler(statement->child_block_);
+    if(statement->tokens_[0] == "listen") port = statement->tokens_[1]
+  }
+}
 
 int main(int argc, char* argv[])
 {
   try {
-    if (argc != 3)
+    if (argc != 2)
     {
-      std::cerr << "Usage: http_server <address> <port>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    receiver 0.0.0.0 80 .\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    receiver 0::0 80 .\n";
+      std::cerr << "Usage: ./webserver <path to config file>\n";
       return 1;
     }
 
-    http::server::server s(argv[1], argv[2]);
+    NginxConfigParser config_parser;
+    NginxConfig config;
+    std::string port;
+    
+    if (config_parser.Parse(argv[1], &config)) {
+      config_handler(&config, port);
+    }
+    else {
+      std::cerr << "Error: Could not parse config file.\n";
+      return 1;
+    }
 
-    std::cout << "server running" << std::endl;
+    http::server::server s("localhost", port);
     s.run();
   }
   catch (std::exception& e) {
