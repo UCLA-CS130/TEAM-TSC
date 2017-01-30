678486b9b65b10a7eb5ae03cb529115ba5ca7a19
Thomas <thomas.chang7@gmail.com>
2017-01-30 05:18:46 +0000

    Update outdated branch with master and remove random git lines

--- src/main.cc
+++ src/main.cc
@@ -19,26 +19,12 @@ int main(int argc, char* argv[])
     config_opts server_config;
     config_handler server_config_handler;
     
-<<<<<<< Updated upstream
-    if (config_parser.Parse(argv[1], &config)) {
-      port = config.GetConfigPort();
-      if(port==""){
-        std::cerr<<"Error: Invalid port number in config file.\n";
-        return 1;
-      }
-    }
-    else {
-      std::cerr << "Error: Could not parse config file.\n";
-      return 1;
-    }
-=======
     //Attempt to fill in server_config based on the config file
     if(!server_config_handler.setup_config(server_config, argv[1]))
       {
 	return 1;
       }
     //We will want to make it so that server takes a config_opts struct as its argument
->>>>>>> Stashed changes
 
     port = server_config.port;
     http::server::server s("0.0.0.0", port);
