10d3c998b5b8c7ed481e05e2331f04b83ae99ad6
Thomas <thomas.chang7@gmail.com>
2017-02-06 00:55:21 -0800

    Update integration test for echo

--- integration_test.py
+++ integration_test.py
@@ -5,26 +5,37 @@ import os
 #subprocess.check_call('make clean; make', stdout=subprocess.PIPE, shell=True)
 
 print('start the webserver')
-webserver = subprocess.Popen(['./build/webserver', 'src/config_file'])
+wr = open('src/integration_config_file', 'w')
+config_contents = 'server {\
+listen 8080;\
+handler_map echo_handler /echo;\
+handler_map static_handler /static1 /static2;\
+base_path /static1 content1;\
+base_path /static2 content2;\
+}'
+wr.write(config_contents)
+wr.close()
+
+webserver = subprocess.Popen(['./build/webserver', 'src/integration_config_file'])
 
 print('send request to server by telnet')
 
-request = 'curl -i localhost:8080'
+request = 'curl -i localhost:8080/echo'
 telnet_proc = subprocess.Popen(request, stdout=subprocess.PIPE, shell=True)
 
 response = telnet_proc.stdout.read().decode('utf-8')
 
-expected_response = 'HTTP/1.1 200 OK\r\n\
-Content-Length: 78\r\n\
+expected_response = 'HTTP/1.0 200 OK\r\n\
+Content-Length: 82\r\n\
 Content-Type: text/plain\r\n\r\n\
-GET / HTTP/1.1\r\n\
+GET /echo HTTP/1.1\r\n\
 User-Agent: curl/7.35.0\r\n\
 Host: localhost:8080\r\n\
 Accept: */*\r\n\r\n'
 
 webserver.kill()
 
-print('check the respones and expected_response')
+print('check the response and expected_response')
 
 '''
 diff = []
