304581f938ed74f79ed2aa963ec302946f449a56
Abineet Das Sharma <abineetds@gmail.com>
2017-03-05 04:12:52 +0000

    completed redirect integration test

--- test/integration_test_redirect.py
+++ test/integration_test_redirect.py
@@ -22,12 +22,21 @@ EXE_PATH = os.path.join(SCRIPT_PATH, '..', 'build', 'webserver')
 
 print(bcolors.OKBLUE + '[==========] ' + bcolors.ENDC + 'start the webserver')
 
-webserver = subprocess.Popen([EXE_PATH, '../' + CONFIG_FILE_DIR + '/config_file'])
+webserver = subprocess.Popen([EXE_PATH, CONFIG_FILE_DIR + '/config_file'])
 
 print(bcolors.OKBLUE + '[----------] ' + bcolors.ENDC + 'send a request to server by curl')
 
-request = 'curl -i localhost:8080'
+request = 'curl -i localhost:8080/'
 curl_proc = subprocess.Popen(request, stdout=subprocess.PIPE, shell=True)
 response = curl_proc.stdout.read().decode('utf-8')
 
-print response
+response = response.split('\r\n')
+if response[0] == "HTTP/1.0 200 OK":
+	print "Redirect integration test passed"
+else:
+	print "Redirect integration test failed"
+	exit(1)
+
+webserver.kill()
+
+
